b9affbf0dc6315b443b08027162859d18be40d7f
demojie <1426134543@qq.com>
2020-04-07 10:56:34 +0800

    first init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Http Server
+
+简单的 Http 服务器，用于检查环境的端口与远程调试。
---
+++ src/main/java/cn/demojie/simplehttpserver/HelloControl.java
@@ -0,0 +1,14 @@
+package cn.demojie.simplehttpserver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloControl {
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
